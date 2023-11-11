6e2724e3bbc7d887fe2bda214512a1ae8783e211
Nadia Tarashkevich <nadia.tarashkevich@jetbrains.com>
2023-11-10 17:43:13 -0800

    Initial commit

---
+++ src/main/java/org/jetbrains/support/ClearRecentProjectsList/ClearRecentProjectsListAction.java
@@ -0,0 +1,51 @@
+package org.jetbrains.support.ClearRecentProjectsList;
+
+import com.intellij.icons.AllIcons;
+import com.intellij.ide.RecentProjectsManager;
+import com.intellij.ide.RecentProjectsManagerBase;
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.ui.Messages;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+import static com.intellij.openapi.ui.MessageConstants.YES;
+
+public class ClearRecentProjectsListAction extends AnAction {
+    final String NAME="Clear all project history";
+    final String REALLY_WANT = "Do you really want to clear list of all projects?";
+    final String SUCCESS="Project history is now empty";
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
+
+        int amSure = Messages.showYesNoDialog(REALLY_WANT,
+                NAME,
+                AllIcons.Actions.DeleteTag);
+
+        if (amSure == YES) {
+            RecentProjectsManagerBase recentProjectsManager = (RecentProjectsManagerBase) RecentProjectsManager.getInstance();
+            List<String> recentPaths = recentProjectsManager.getRecentPaths();
+            for (String recentPath : recentPaths) {
+                recentProjectsManager.removePath(recentPath);
+            }
+            String groupId = NAME;
+
+            // Create the notification
+            Notification notification = new Notification(
+                    groupId,
+                    NAME,
+                    SUCCESS,
+                    NotificationType.INFORMATION
+            );
+
+            // Show the notification
+            Notifications.Bus.notify(notification, anActionEvent.getProject());
+        }
+
+    }
+}
